1ebf442769b19122d4bc26bbf335ea80d682bbce
danielcfuentes <danielscollege04@gmail.com>
2024-04-18 19:53:25 -0600

    finished producer

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/producer_interface.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/producer_interface.py
@@ -16,17 +16,18 @@
 class mqProducerInterface:
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         # Save parameters to class variables
-
+    
         # Call setupRMQConnection
+        
         pass
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
-
+        
         # Establish Channel
-
+        
         # Create the exchange if not already present
-
+        
         pass
 
     def publishOrder(self, message: str) -> None:
